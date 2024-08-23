a80b8fb63b3d54e4d0d72e534f7050a0874fe41c
gabdemiranda <gabriel.demcastro@gmail.com>
2024-08-23 19:52:01 -0300

    User can register and edit a thought

--- js/api.js
+++ js/api.js
@@ -8,6 +8,45 @@ const api = {
       alert('Erro ao buscar pensamentos')
       throw erroor
     }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+      return await response.json()
+    } catch {
+      alert("Erro aao buscar pensamento")
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(pensamento)
+      })
+      return JSON.stringify(pensamento)
+    } catch {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw erroor
+    }
   }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos()
+
+  const formPensamento = document.querySelector("#pensamento-form")
+  formPensamento.addEventListener("submit", manipularSubmissaoForm)
+
+  const botaoCancelar = document.querySelector("#botao-cancelar")
+  botaoCancelar.addEventListener("click", manipularCancelamento)
 })
+
+async function manipularSubmissaoForm(event) {
+  event.preventDefault()
+  const id = document.querySelector("#pensamento-id").value
+  const conteudo = document.querySelector("#pensamento-conteudo").value
+  const autoria = document.querySelector("#pensamento-autoria").value
+
+  try {
+    if(id) {
+      await api.editarPensamento({ id, conteudo, autoria })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria })
+    }
+    ui.renderizarPensamentos()
+    manipularCancelamento()
+  } catch {
+    alert("Erro ao salvar pensamento")
+  }
+}
+
+function manipularCancelamento() {
+  ui.limparForm()
+}
